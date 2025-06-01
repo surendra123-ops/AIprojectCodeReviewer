638ceeb7da4209422fd708d3937c0e5c1698fed6
surendrakumarreddy79@gmail.com <surendrayenika@gmail.com>
2025-06-01 16:13:39 +0530

    Backend ready for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,8 @@
-
-
 const app = require('./src/app');
 
+// Use PORT from environment variables or default to 5000
+const PORT = process.env.PORT || 5000;
 
-
-
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-} );
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
